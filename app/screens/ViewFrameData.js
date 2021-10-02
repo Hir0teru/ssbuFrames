009475a912130232024f554647c8a87831481aad
hiroteru <78360123+HiraiDaiki@users.noreply.github.com>
2021-10-02 16:52:07 +0900

    screens/ViewFrameDataを定義

---
+++ app/screens/ViewFrameData.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View} from 'react-native';
+
+const ViewFrameData = ({route}) => {
+	const {id, name} = route.params;
+  return (
+    <View>
+    </View>
+  );
+}
+
+export default ViewFrameData;
