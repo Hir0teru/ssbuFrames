1c82e21decd23e899d38493940cccb4ba02e6b29
hiroteru <78360123+HiraiDaiki@users.noreply.github.com>
2021-10-03 17:05:58 +0900

    App.jsでcomponents/TabNavigatorを呼び出し、タブによるscreen間の遷移を可能とする修正

--- app/App.js
+++ app/App.js
@@ -4,13 +4,22 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import SelectCharacter from './screens/SelectCharacter';
 import ViewFrameData from './screens/ViewFrameData';
+import TabNavigator from './components/TabNavigator';
 import Settings from './screens/Settings';
 
 const Stack = createStackNavigator();
+
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">{/* (1) */}
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Tab"
+          component={TabNavigator}
+          options={({route}) => ({
+            headerShown: false
+          })}
+        />
         <Stack.Screen
           name="SelectCharacter"
           component={SelectCharacter}
