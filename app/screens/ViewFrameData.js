c1d52e92c83f86a61867c4cdd5aba17f49e2b98b
hiroteru <78360123+HiraiDaiki@users.noreply.github.com>
2021-11-20 17:49:36 +0900

    screens/ViewFrameDataでfirestoreから非同期でフレームデータを取得しコンポーネントに渡す処理を実装（暫定対応）

--- app/screens/ViewFrameData.js
+++ app/screens/ViewFrameData.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import {View} from 'react-native';
-import { useColorContext, colorModes } from '../context/ColorMode'
+import React, { useState, useEffect } from 'react';
+import { useColorContext, colorModes } from '../context/ColorMode';
+import db from '../db/firebase';
+import TopTabNavigator from '../components/TopTabNavigator';
 
 const ViewFrameData = ({route}) => {
 	const {id, name} = route.params;
+  const [frames, setFrames] = useState({});
+  useEffect(() => {
+    fetchFrames();
+  }, []);
+
+  // firestoreからデータを取得し、stateにセットする
+  const fetchFrames = async() => {
+    const snapShot = await db.collection('characters').doc('1').get(); //TODO:動的にする
+    setFrames(snapShot.data());
+  }
   return (
-    <View>
-    </View>
+    <TopTabNavigator frames={frames}/>
   );
 }
 
