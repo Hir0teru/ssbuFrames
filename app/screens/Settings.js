0a414fb7fb2884992d60c5ba33b66038df1a85e4
hiroteru <78360123+HiraiDaiki@users.noreply.github.com>
2021-11-27 15:52:42 +0900

    screens/Settingsで言語の切り替えを可能とする修正

--- app/screens/Settings.js
+++ app/screens/Settings.js
@@ -2,10 +2,11 @@ import React from 'react';
 import {View} from 'react-native';
 import { RadioButton, Divider, Card } from 'react-native-paper';
 import { useColorContext, colorModes } from '../context/ColorMode';
-
+import { useTranslation } from 'react-i18next';
 
 const Settings = ({route}) => {
   const { color, setColor } = useColorContext()
+  const { t, i18n} = useTranslation()
   return (
     <View>
       <Card>
@@ -19,13 +20,12 @@ const Settings = ({route}) => {
       <Divider/>
       <Card>
         <Card.Title title="言語" subtitle="language"/>
-        <RadioButton.Group>
-          <RadioButton.Item label="日本語" value="japanese" />
-          <RadioButton.Item label="English" value="english" />
+        <RadioButton.Group onValueChange={value => i18n.changeLanguage(value)} value={i18n.resolvedLanguage} >
+          <RadioButton.Item label="日本語" value="ja" />
+          <RadioButton.Item label="English" value="en" />
         </RadioButton.Group>
       </Card>
     </View>
-
   );
 }
 
